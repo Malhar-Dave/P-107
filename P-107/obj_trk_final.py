0963d4e7d0d18a90784aaab409ee39cdf1357db5
Malhar-Dave <97378784+Malhar-Dave@users.noreply.github.com>
2023-06-02 17:10:08 +0930

    Add files via upload

--- P-107/obj_trk_final.py
+++ P-107/obj_trk_final.py
@@ -46,32 +46,23 @@ def drawBox(img,bbox):
     cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
 
 
-
-def drawBox(img,bbox):
-    x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
-    cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
-    cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-    pass
-
-
 while True:
    check,img = video.read()
    success.bbox = tracker.update(img)
+   
+    if success:
+        drawBox(img,bbox)
+    else:
+        cv2.putText(img,"Lost", (75,90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+
+    goal_track(img, bbox)
+    cv2.imshow(img)
 
 
-if success:
-    drawBox(img,bbox)
-else:
-    cv2.putText(img,"Lost", (75,90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
-
-goal_track(img, bbox)
-cv2.imshow(img)
-
-
-key = cv2.waitKey(1)
-if key == ord('q'):
-    print("Closing")
-    break
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        print("Closing")
+        break
 
 
 video.release()
